344a4c58586b05d064b148aa154860a2a8627467
rlaquea <rlaquea14@gmail.com>
2021-07-03 22:28:09 -0400

    added banner and title, search box form functionality and began functions for local storage of city searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,42 @@
 var apiKey = "49eb16dfa1cec272cd9a1afb144b9378"
-
-
 var searchBtn = document.querySelector("#search-button")
-
-
 //target form for search input
-    function searchInput () {
-        var searchInput = document.querySelector("#search-input").value;
-        console.log(searchInput);
-    }
-
-
-// search for the current weather using api
-
-function getWeatherNow(searchInput) {
-    var queryUrl = "api.openweathermap.org/data/2.5/weather?q=" + searchInput + "$appid=" + apiKey; 
+function searchInput() {
+    var searchInput = document.querySelector("#search-input").value;
+    console.log(searchInput);
 }
 
-// var weatherDashUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=94.04&exclude=part&appid=49eb16dfa1cec272cd9a1afb144b9378"
-
-
-
-fetch(weatherDashUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    });
-
-
+// search for the current weather using api
+function getWeatherNow(searchInput) {
+    var weatherDataUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "$appid=" + apiKey;
+    fetch(weatherDataUrl)
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+}
 //use onecall and current weather data 
+
+function saveCitySearch() {
+    var citySearch = JSON.parse(window.localStorage.getItem("#search-input")) || []
+    var savedCities = {
+        name: city,
+        input: savedCities
+    }
+   citySearch.push(savedCities)
+    window.localStorage.setItem("saved-city", JSON.stringify
+    (citySearch))
+    event.preventDefault();
+}
+   
+
+searchBtn.addEventListener("click", searchInput)
+//this will display the saved city searches on the page
+    // savedCities.textContent = userInput.savedCities;
+   // savedCities.appendChild(userInput);
+    // savedCities.push(userScore)
+    // window.localStorage.setItem("user-input", JSON.stringify
+    // (savedScores))
+    // 
